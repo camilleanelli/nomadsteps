066a6421fda6c655c383c678f80893c578c17890
Camille Anelli <camilleanelli32@gmail.com>
2016-08-10 19:40:02 +0200

    config routes for trips

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/trips' => 'trips#index'
+  resources :trips, only: [:index, :new, :create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
