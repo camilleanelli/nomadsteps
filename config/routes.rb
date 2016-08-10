9d792e4a626d8bd6064ffda2b542f47e6560c223
Camille Anelli <camilleanelli32@gmail.com>
2016-08-10 20:39:59 +0200

    configuration of devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'trips#index'
+
   resources :trips, only: [:index, :new, :create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
