69fd42f2626ffcc312f90b2625699aa5f0f6dbc0
Camille Anelli <camilleanelli32@gmail.com>
2016-08-17 15:02:04 +0200

    add model accomodation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160816095012) do
+ActiveRecord::Schema.define(version: 20160817124826) do
+
+  create_table "accomodations", force: :cascade do |t|
+    t.string   "accomodation_type"
+    t.date     "start_date"
+    t.date     "end_date"
+    t.decimal  "price"
+    t.string   "link"
+    t.integer  "number_of_persons"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+  end
 
   create_table "transportations", force: :cascade do |t|
     t.string   "transportation_type"
