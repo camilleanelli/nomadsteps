5686d1165fdab519d5df765a49b2f2c28d0b9fe2
Camille Anelli <camilleanelli32@gmail.com>
2016-08-18 17:45:05 +0200

    add method to calculate prices and nights

--- app/models/accomodation.rb
+++ app/models/accomodation.rb
@@ -1,3 +1,19 @@
 class Accomodation < ApplicationRecord
   belongs_to :trip
+
+  def count_night
+    (self.end_date - self.start_date).to_i
+  end
+
+  def price_per_personne
+    self.price / self.number_of_persons
+  end
+
+  def price_per_person_per_night
+    (self.price / self.count_night) / self.number_of_persons
+  end
+
+  def total_accomodations
+    self.price * self.count
+  end
 end
