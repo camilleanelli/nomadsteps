f6be745d0675288bb18ab5920122c6241137fbf1
Camille Anelli <camilleanelli32@gmail.com>
2016-08-18 13:50:06 +0200

    add validations to the model

--- app/models/transportation.rb
+++ app/models/transportation.rb
@@ -1,3 +1,9 @@
 class Transportation < ApplicationRecord
   belongs_to :trip
+
+  validates :transportation_type, presence: true
+  validates :departure_datetime, presence: true
+  validates :arrival_time, presence: true
+  validates :reference_number, presence: true
+
 end
