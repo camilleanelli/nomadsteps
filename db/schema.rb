d50ae2855b7b4f734626b1ecb3d89fe9f4147184
Camille Anelli <camilleanelli32@gmail.com>
2016-08-18 14:47:32 +0200

    fix database

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20160817132853) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "accomodations", force: :cascade do |t|
     t.string   "accomodation_type"
     t.date     "start_date"
@@ -22,7 +25,7 @@ ActiveRecord::Schema.define(version: 20160817132853) do
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
     t.integer  "trip_id"
-    t.index ["trip_id"], name: "index_accomodations_on_trip_id"
+    t.index ["trip_id"], name: "index_accomodations_on_trip_id", using: :btree
   end
 
   create_table "transportations", force: :cascade do |t|
@@ -36,7 +39,7 @@ ActiveRecord::Schema.define(version: 20160817132853) do
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
     t.integer  "trip_id"
-    t.index ["trip_id"], name: "index_transportations_on_trip_id"
+    t.index ["trip_id"], name: "index_transportations_on_trip_id", using: :btree
   end
 
   create_table "trips", force: :cascade do |t|
@@ -70,8 +73,10 @@ ActiveRecord::Schema.define(version: 20160817132853) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "nomadlist_username"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "accomodations", "trips"
+  add_foreign_key "transportations", "trips"
 end
