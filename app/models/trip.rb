4fa8bcb2b9f480c2ad08cddaa3c85a457af9150c
Camille Anelli <camilleanelli32@gmail.com>
2016-08-21 15:32:26 +0200

    add number of day

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,4 +2,8 @@ class Trip < ApplicationRecord
   has_many :transportations, dependent: :destroy
   has_many :accomodations, dependent: :destroy
   has_and_belongs_to_many :users
+
+  def days_number
+    (self.end_date - self.start_date).to_i
+  end
 end
