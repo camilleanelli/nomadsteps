d56a1305076074cc1b0c776a07946d82a8a15adf
Camille Anelli <camilleanelli32@gmail.com>
2016-08-22 15:57:14 +0200

    change import nomdalist in json import

--- app/controllers/import_nomad_lists_controller.rb
+++ app/controllers/import_nomad_lists_controller.rb
@@ -1,21 +1,17 @@
-# supprime camilleanelli hardcodé, ça doit venir du user connecté
-# boucler sur le tableau de hash de nomad trips, et créer des Trip (modele)
-# ces trips doivent être associés au current users
-
-# bonus
-# changer l'action pour un create
-# déclencher plusieurs fois l'action n'est pas censé créer plusieurs fois les même Trip
-# création en tâche de fond
 class ImportNomadListsController < ApplicationController
   def index
-    @url = ImportTrip.new(current_user.nomadlist_username)
-    @trips = @url.table_import.each do |hash|
-      current_user.trips.find_or_create_by(start_date: hash["start_date"],
-        end_date: hash["end_date"],
-        destination: hash["city"],
-        longitude: hash["longitude"],
-        latitude: hash["latitude"])
-    end
-    redirect_to root_path
+
+      @url = ImportJsonTrip.new(current_user.nomadlist_username)
+      @trips = @url.nomadlist_table.each do |hash|
+        current_user.trips.find_or_create_by(start_date: hash["date_start"],
+                                             end_date: hash["date_end"],
+                                             destination: hash["place"],
+                                             image_trip: hash["place_photo"],
+                                             country: hash["country"],
+                                             longitude: hash["longitude"],
+                                             latitude: hash["latitude"]
+                                             )
+      end
+      redirect_to root_path
   end
 end
