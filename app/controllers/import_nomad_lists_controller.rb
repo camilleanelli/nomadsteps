bfe27c3862c64f5b85e0d2a5a24ffdf02f87eef5
Camille Anelli <camilleanelli32@gmail.com>
2016-08-22 19:54:26 +0200

    add image to the edit trip view

--- app/controllers/import_nomad_lists_controller.rb
+++ app/controllers/import_nomad_lists_controller.rb
@@ -1,12 +1,11 @@
 class ImportNomadListsController < ApplicationController
   def index
-
       @url = ImportJsonTrip.new(current_user.nomadlist_username)
       @trips = @url.nomadlist_table.each do |hash|
         current_user.trips.find_or_create_by(start_date: hash["date_start"],
                                              end_date: hash["date_end"],
                                              destination: hash["place"],
-                                             image_trip: hash["place_photo"],
+                                             image_trip: "https://nomadlist.com" + hash["place_photo"],
                                              country: hash["country"],
                                              longitude: hash["longitude"],
                                              latitude: hash["latitude"]
