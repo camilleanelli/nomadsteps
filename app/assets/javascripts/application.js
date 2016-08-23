82cdbfbd7b0fe6aa20949011864f5c0751989229
Camille Anelli <camilleanelli32@gmail.com>
2016-08-23 11:51:39 +0200

    fix controller tickets to add strong parameters and actions new create

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require tether
-
+//= require turbolinks
 //= require bootstrap
 //= require jquery_ujs
 //= require_tree .
--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -4,14 +4,26 @@ class TicketsController < ApplicationController
   end
 
   def new
-    @trip = current_user.trips.params[:trip_id]
-    @transportation = @trip.transportations.params[:transportation_id]
+    @trip = current_user.trips.find(params[:trip_id])
+    @transportation = @trip.transportations.find(params[:transportation_id])
     @ticket = @transportation.tickets.new
   end
 
   def create
+    @trip = current_user.trips.find(params[:trip_id])
+    @transportation = @trip.transportations.find(params[:transportation_id])
+    @ticket = @transportation.tickets.new(params_tickets)
+    if @ticket.save!
+      redirect_to trip_transportation_path(@transportation)
+    else
+      render :new
+    end
   end
 
   private
+
+  def params_tickets
+    params[:ticket].permit(:cloudinary_id, :transportation_id)
+  end
 
 end
