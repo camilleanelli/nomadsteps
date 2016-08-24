af8c09b6bcb31588c33c4a7aa0bb0b8758697d63
Camille Anelli <camilleanelli32@gmail.com>
2016-08-24 16:14:41 +0200

    add the possibility to upload tickets

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Nomadtrip
   class Application < Rails::Application
+
+    config.filepicker_rails.api_key = "AyZJE5pdkRlWRbgNAiyQ3z"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160822182031) do
+ActiveRecord::Schema.define(version: 20160824111817) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20160822182031) do
     t.string   "cloudinary_id"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
+    t.string   "filepicker_url"
     t.index ["transportation_id"], name: "index_tickets_on_transportation_id", using: :btree
   end
 
