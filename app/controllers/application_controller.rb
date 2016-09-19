68d0ef3260ffbb80e61260572c68544a13402053
Camille Anelli <camilleanelli32@gmail.com>
2016-09-19 11:19:18 +0100

    redirect to trips path after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   protect_from_forgery with: :exception
 
+  def after_sign_in_path_for(resource)
+    trips_path
+  end
   protected
 
   def configure_permitted_parameters
