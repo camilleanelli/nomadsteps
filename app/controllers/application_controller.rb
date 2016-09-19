605b43237967b7fd2961f9bd3a3052fab5fa33ca
Camille Anelli <camilleanelli32@gmail.com>
2016-09-19 13:08:05 +0100

    config invitable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     trips_path
   end
+
   protected
 
   def configure_permitted_parameters
---
+++ app/controllers/invitations_controller.rb
@@ -0,0 +1,8 @@
+class InvitationsController < Devise::InvitationsController
+  
+  def edit
+  end
+
+  def new
+  end
+end
