487d0373285b47a793346d04dbc2ab5f87bb4ff9
Camille Anelli <camilleanelli32@gmail.com>
2016-10-12 17:28:23 +0200

    add feature to invite user for a trip

--- config/routes.rb
+++ config/routes.rb
@@ -10,11 +10,14 @@ Rails.application.routes.draw do
   patch 'profile', to: "profile#update", as: "profile"
 
   resources :trips, except: [:show] do
+    resources :invitations, only: [:new, :create]
+
     resources :transportations do
       resources :tickets, only: [:new, :create, :destroy]
     end
     resources :accomodations
-    end
-    resources :import_nomad_lists
   end
+  resources :import_nomad_lists
+
+end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
