4b9351209b93c6a06955e0f98353ccc96d46adec
Camille Anelli <camilleanelli32@gmail.com>
2016-10-19 12:45:01 +0200

    add avatar in navbar application layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def avatar_navbar
+    if current_user.filepicker_url?
+      filepicker_image_tag current_user.filepicker_url, {w: 25, h: 25, fit: 'crop'}, class:"img-circle"
+    else
+      "<i class='fa fa-user' aria-hidden='true'></i>"
+    end
+  end
 end
