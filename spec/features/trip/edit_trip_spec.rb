4a90e5761db84970ddb28d784fb18f843a27a6e3
camille anelli <camilleanelli32@gmail.com>
2016-10-20 11:40:32 +0200

    Instagram account (#105)
    * add instagram account
    
    * test create a trip
    
    * delete empty files

--- spec/features/trip/edit_trip_spec.rb
+++ spec/features/trip/edit_trip_spec.rb
@@ -1,22 +1,21 @@
-require 'rails_helper'
-
-RSpec.describe "edit a trip", @javascript, :type => :feature do
-  it 'redirect to index of trips' do
-    user = User.create(first_name: "John", last_name: "Doe", email: 'johndoe@email.com', password: 'password', confirmed_at: 1.day.ago)
-    trip =  Trip.create(destination: "Paris", start_date: "24/11/2016", end_date: "30/11/2016", city_name: "Paris", country_name: "France", person_number: 2)
-    user.trips << trip
-    visit "/"
-
-    click_link "Login"
-    fill_in "Email", with: "johndoe@email.com"
-    fill_in "Password", with: "password"
-    click_button "Validate"
-
-    visit '/trips'
-    click_link 'Paris '
-
-
-
-
-  end
-end
+# require 'rails_helper'
+#
+# RSpec.describe "edit a trip", @javascript, :type => :feature do
+#   it 'redirect to index of trips' do
+#     user = User.create(first_name: "John", last_name: "Doe", email: 'johndoe@email.com', password: 'password', confirmed_at: 1.day.ago)
+#     trip =  Trip.create(destination: "Paris", start_date: "24/11/2016", end_date: "30/11/2016", city_name: "Paris", country_name: "France", person_number: 2)
+#     user.trips << trip
+#       visit "/"
+#     click_link "Login"
+#     fill_in "Email", with: "johndoe@email.com"
+#     fill_in "Password", with: "password"
+#     click_button "Validate"
+#
+#     visit '/trips'
+#     click_link 'Paris '
+#
+#
+#
+#
+#   end
+# end
