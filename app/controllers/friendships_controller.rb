155792233498dc96c3ca3a7ca46c3cc73a658e3b
Camille Anelli <camilleanelli32@gmail.com>
2016-10-24 12:51:25 +0200

    fix bug invitation friend and index friends if invit is not confirmed

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,6 +1,5 @@
 class FriendshipsController < AuthenticatedController
   def index
-
     @friends = current_user.friends
     @inverse_friends = current_user.inverse_friends
   end
@@ -11,26 +10,33 @@ class FriendshipsController < AuthenticatedController
   end
 
   def create
-    @friend = User.find_by(params_friend)
-    if @friend.nil?
-      @friend.invite!(params_friend)
-    else
+    @friend = User.find_or_create_by(params_friend)
       @friend.invite!
-    end
-    current_user.friends << @friend
-    flash[:notice] = "Your friend has been successfully added to your account"
-    redirect_to trips_path
+      current_user.friends << @friend
+      flash[:notice] = "Your friend has been successfully added to your account"
+      redirect_to trips_path
   end
 
   def destroy
     @friendships = current_user.friendships.where(friend_id: params[:id])
+    @inverse_friendships = current_user.inverse_friendships.where(user_id: params[:id])
     @trips = current_user.trips
     @friendships.each do |friendship|
       friendship.destroy
       @trips.each do |trip|
          trip.users.each do |user|
            if user.id == friendship.friend.id
-             trip.users.delete(user.id)
+             trip.users.delete(user)
+           end
+         end
+      end
+    end
+    @inverse_friendships.each do |inverse_friendship|
+      inverse_friendship.destroy
+      @trips.each do |trip|
+         trip.users.each do |user|
+           if user.id == inverse_friendship.user.id
+             trip.users.delete(user)
            end
          end
       end
