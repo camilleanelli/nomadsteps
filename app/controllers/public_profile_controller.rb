9815c6d4e919db7d3f8d243734f91d39722b9b73
Camille Anelli <camilleanelli32@gmail.com>
2016-11-15 15:51:13 +0700

    add option to edit a profil

--- app/controllers/public_profile_controller.rb
+++ app/controllers/public_profile_controller.rb
@@ -1,11 +1,13 @@
 class PublicProfileController < ApplicationController
+  skip_before_action :authenticate_user!
 
   def show
     @user = User.find(params[:user_id])
     @trips = @user.trips.order(start_date: :desc).includes(:trips_users, :users, :accomodations, :transportations)
+    @current_trip = @user.current_trip
   end
 
   private
 
-  
+
 end
