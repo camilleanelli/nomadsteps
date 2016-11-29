26091ec7efcca3f56384b2c3e45f6f9b0ad077fe
Camille Anelli <camilleanelli32@gmail.com>
2016-11-29 19:41:22 +0700

    add social links

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+
 
   get 'dashboard', to: 'dashboard#show', as: "dashboard"
 
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
 
   get 'public_profile/:user_id', to: "public_profile#show", as: "public_profile"
   resources :trips, except: [:show] do
-    resources :transportations do
+    resources :transportations, except: [:show] do
       resources :tickets, only: [:new, :create, :destroy]
     end
     resources :accomodations
