36fbcc9e77f534c37aaa26def1968d3c8a865868
Camille Anelli <camilleanelli32@gmail.com>
2017-03-07 13:46:33 +0700

    replace parsing by scraping

--- app/controllers/import_nomad_lists_controller.rb
+++ app/controllers/import_nomad_lists_controller.rb
@@ -1,34 +1,17 @@
 class ImportNomadListsController < AuthenticatedController
   def index
-  #   @url = ImportJsonTrip.new(current_user.nomadlist_username)
-  #   @trips = @url.nomadlist_table.each do |hash|
-  #     current_user.trips.find_or_create_by(start_date: hash["date_start"],
-  #                                          end_date: hash["date_end"],
-  #                                          destination: hash["place"],
-  #                                          image_trip: "https://nomadlist.com" + hash["place_photo"],
-  #                                          longitude: hash["longitude"],
-  #                                          latitude: hash["latitude"],
-  #                                          person_number: 1,
-  #                                          city_name: hash["place"],
-  #                                          country_name: hash["country"]
-  #                                          )
-  #   end
-  #   redirect_to trips_path
-  # end
-
-
-  @url = Nomadlist.new(current_user.nomadlist_username)
-  @nomadlist_table = @url.table_import
-  @nomadlist_table.each do |hash|
-    current_user.trips.find_or_create_by(start_date: hash["start_date"],
+    @url = NomadlistImport.new(current_user.nomadlist_username)
+    @nomadlist_table = @url.table_import
+    @nomadlist_table.each do |hash|
+      current_user.trips.find_or_create_by(start_date: hash["start_date"],
                                           end_date: hash["end_date"],
                                           city_name: hash["city"],
                                           country_name: hash["country"],
                                           longitude: hash["longitude"],
                                           latitude: hash["latitude"]
                                           )
+    end
+    redirect_to trips_path
   end
-  redirect_to trips_path
-end
 
 end
